954eb1b90c34f8582e66858a62b2850018260811
Bogdan <bogdan.pettrovic@gmail.com>
2025-01-18 00:33:51 +0100

    Initial database draft

---
+++ server/db/db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  surname VARCHAT(30) NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  phone_number TEXT NOT NULL UNIQUE,
+  date_of_birth DATE,
+  password TEXT NOT NULL
+);
+SELECT setval('users_id_seq', 100000, false);
+
+CREATE TABLE user_finances (
+  user_id BIGINT NOT NULL REFERENCES users(id),
+  balance DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+  debt DECIMAL(10, 2) NOT NULL DEFAULT 0.00
+);
+
+CREATE TABLE transactions (
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  sender_id BIGINT NOT NULL REFERENCES users(id),
+  reciver_id BIGINT NOT NULL REFERENCES users(id)
+  amount DECIMAL(10, 2) NOT NULL
+);
